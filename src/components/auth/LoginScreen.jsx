79d26201b44ec336750844cdcdeb7285c8b70b9f
Jose Junior <jose.junior@porlande.com>
2022-02-01 06:54:30 +0100

    start dashboard

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,16 +1,49 @@
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/styles';
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography';
+
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Icon }  from '@material-ui/core'
+
+
+const useStyles = makeStyles((theme) => ({
+
+  title: {
+    flexFlow : 1
+  },
+}));
 
 export const LoginScreen = () => {
+  const classes = useStyles();
     return ( 
         <>
+        
+        <div >
+      <AppBar position="static" >
+        <Toolbar  disableElevation>
+          <IconButton edge="start" color="inherit" aria-label="menu" className={classes.title}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6"  className={classes.title}  align='center'>
+            News
+          </Typography>
+          <Button color="inherit"  align='center'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
         <h1>Login SCREEN</h1>
 
         <Stack spacing={2} direction="row">
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disableElevation>Contained</Button>
       <Button variant="outlined">Outlined</Button>
     </Stack>
+      <Typography variant="body1" color="secondary" align='center'>Typo test</Typography>
+    
         </>
      );
 }
